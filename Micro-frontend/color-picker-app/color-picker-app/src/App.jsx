6a0b830b140ca2df5cfee59d91eeda5ba5789559
Marielsgtzz <marielsgtzz@gmail.com>
2023-06-04 18:18:14 -0700

    Add reactive color picker

--- Micro-frontend/color-picker-app/color-picker-app/src/App.jsx
+++ Micro-frontend/color-picker-app/color-picker-app/src/App.jsx
@@ -4,8 +4,17 @@ import ColorPicker from "colorPicker/ColorPicker";
 import "./index.css";
 
 const App = () => (
-	<div className="container">
-		<ColorPicker />
-	</div>
+	<>
+		<h1 className="text-center bg-dark text-white p-2">Color Picker</h1>
+		<div className="container mt-4">
+			<div className="row">
+				<div className="col-12 col-md-6">Lista de colores</div>
+				<div className="col-12 col-md-6">
+					<ColorPicker />
+				</div>
+			</div>
+		</div>
+		{/* <ColorPicker /> */}
+	</>
 );
 ReactDOM.render(<App />, document.getElementById("app"));
--- Micro-frontend/color-picker-app/mf-picker/src/components/ColorPicker.jsx
+++ Micro-frontend/color-picker-app/mf-picker/src/components/ColorPicker.jsx
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
 const ColorPicker = () => {
-	return <div>Color picker</div>;
+	const [color, setColor] = useState("#000000");
+	const handleChangeColor = (e) => {
+		setColor(e.target.value);
+	};
+	return (
+		<form>
+			<input
+				type="color"
+				className="form-control"
+				style={{ width: "100%", height: "200px" }}
+				value={color}
+				onChange={handleChangeColor}
+			/>
+			<div className="text-center">
+				<h2 className="mt-3 fw-bolder">
+					<div
+						style={{
+							width: "20px",
+							height: "20px",
+							background: color,
+							display: "inline-block",
+							margin: "0 1rem",
+							borderRadius: "100%",
+						}}
+					></div>
+					{color}
+				</h2>
+			</div>
+		</form>
+	);
 };
 
 export default ColorPicker;
